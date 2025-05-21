c4f70eb6e46873b859d75587fcca0894ac17daed
Francisco Júnior <franciscoamj1001@gmail.com>
2025-05-20 21:54:13 -0300

    Primeira parte do projeto

---
+++ models/users.js
@@ -0,0 +1,33 @@
+const { default: mongoose } = require("mongoose");
+
+const mongoose = require(mongoose);
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        require: true,
+    },
+
+    email: {
+        type: String,
+        require: true,
+    },
+
+    phone: {
+        type: String,
+        require: true,
+    },
+
+    image: {
+        type: String,
+        require: true,
+    },
+
+    created: {
+        type: Date,
+        require: true,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("User", userSchema);
